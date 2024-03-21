b97537ebab481c0fb6020b38a45015b097fe8fc3
Zulai Romero <zulairomero@gmail.com>
2024-03-21 15:44:02 -0400

    add dependencies, properties & styling

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -3,8 +3,48 @@ import React, {
     MouseEventHandler,
     ForwardRefRenderFunction,
     PropsWithChildren,
-}, from 'react';
+} from 'react';
+import styled from '@emotion/styled';
+import { IconName } from '../../icons/icon-constant';
+export  type TileProps = {
+    children: React.ReactNode;
+    disabled?: boolean;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+    icon: IconName;
+    rightIcon: boolean
+};
 
-importt
 
-// create two tiles, one Default, and another active.
+const StyledTile = styled.button<TileProps>`
+position:relative;
+border:none;
+display: inline-block;
+box-sizing: border-box;
+outline: none;
+font-weight: 600;
+font-size: 16px;
+line-height: 22px;
+cursor: pointer;
+
+
+${({
+    disabled,
+    theme: {
+        semantic: { border, spacing, color },
+    },
+}) => 
+    `
+    border-radius: ${border.radius.pill};
+    padding: ${spacing.spacingM};
+    background-color: ${
+        !disabled ? color.bg.default : color.bg.disabled}
+    
+    `}  
+
+
+    
+
+}`
+
+
+export default forwardRef(Tile);
