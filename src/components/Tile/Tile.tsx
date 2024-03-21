07d06ddba2b7b9febde30894328ae5488e4dab7a
Zulai Romero <zulairomero@gmail.com>
2024-03-21 13:27:47 -0400

    First commit

---
+++ src/components/Tile/Tile.tsx
@@ -0,0 +1,10 @@
+import React, {
+    forwardRef,
+    MouseEventHandler,
+    ForwardRefRenderFunction,
+    PropsWithChildren,
+}, from 'react';
+
+importt
+
+// create two tiles, one Default, and another active.
